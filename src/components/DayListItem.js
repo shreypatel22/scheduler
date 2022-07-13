05c092907c81f0f38f1c9fdde24c4993bf10ef13
Shrey Patel <shreypatel11@hotmail.com>
2022-07-13 00:35:12 -0400

    Refactor DayListItem to show correct message for spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,12 +4,26 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   const [day, setDay] = useState(props.name);
-  const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": !props.spots})
+  const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": !props.spots});
+
+  const formatSpots = () => {
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+
+    if (props.spots === 1) {
+      return `1 spot remaining`;
+    }
+    
+    if (props.spots === 0) {
+      return `no spots remaining`;
+    }
+  }
   
   return (
     <li onClick={() => props.setDay(day)} className={dayClass}>
       <h2 className="text--regular">{day}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
