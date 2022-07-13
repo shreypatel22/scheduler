46c2b990793745a0788cde27798a4be8c5794af0
Shrey Patel <shreypatel11@hotmail.com>
2022-07-13 15:54:32 -0400

    Modify DayListItem to remove useState

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,6 @@ import "./DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  const [day, setDay] = useState(props.name);
   const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": !props.spots});
 
   const formatSpots = () => {
@@ -21,8 +20,8 @@ export default function DayListItem(props) {
   }
   
   return (
-    <li onClick={() => props.setDay(day)} className={dayClass}>
-      <h2 className="text--regular">{day}</h2> 
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
