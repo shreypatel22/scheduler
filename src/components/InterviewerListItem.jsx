1c4dee8445699111b7327a600806851d35a76df5
Shrey Patel <shreypatel11@hotmail.com>
2022-07-14 15:44:27 -0400

    Refactor setInterviewer for IntervieweListItem and InterviewerList

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -9,7 +9,7 @@ export default function InterviewerListItem(props) {
 
 
   return (
-    <li onClick={() => setInterviewer(id)} className={interviewerClass}>
+    <li onClick={setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={avatar}
