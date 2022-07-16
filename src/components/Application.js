1c5968bd3cb22242b65be427008c9e0c75ad20f5
Shrey Patel <shreypatel11@hotmail.com>
2022-07-16 16:48:41 -0400

    Create state object to store all states on Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,13 +46,21 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day }); 
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get(`http://localhost:8001/api/days`).then(res => {
-      setDays([...res.data])
-      console.log(res.data)
+      setDays([...res.data])      
     });
   }, []);
 
@@ -76,8 +84,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
