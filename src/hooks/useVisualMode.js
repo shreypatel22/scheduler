7738629cb817d78dedd7fbc76e1f89fc3d55447e
Shrey Patel <shreypatel11@hotmail.com>
2022-07-17 01:06:40 -0400

    Create Custom Hook for transitioning through appointment modes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      const newHistory = [...history];
+      newHistory.pop();
+      setHistory(newHistory)
+    }
+    
+    setMode(newMode)
+    setHistory((prev) => [...prev, newMode])
+    
+  }
+
+  function back() {
+    if (history.length <= 1) {
+      return;
+    }
+
+    const newHistory = [...history];
+    newHistory.pop()
+    const prevMode = newHistory[newHistory.length - 1];
+
+    setMode(prevMode);
+    setHistory(newHistory);
+  }
+
+  return {mode, transition, back}
+}
