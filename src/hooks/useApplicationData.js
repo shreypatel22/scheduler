1ac7de4e9b3743451758639ed611b0c81b4e8860
Shrey Patel <shreypatel11@hotmail.com>
2022-07-20 13:36:09 -0400

    Update proxy

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,10 +15,10 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get(`http://localhost:8001/api/days`),
-      axios.get(`http://localhost:8001/api/appointments`),
-      axios.get(`http://localhost:8001/api/interviewers`)
-    ]).then((all) => {
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
+    ]).then((all) => {      
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));  
     })
   }, []);
@@ -50,7 +50,7 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    return axios.put(`/api/appointments/${id}`, {interview})
       .then(() => {
         const days = updateSpots(state, true);
         setState({
@@ -72,7 +72,7 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, {appointment})
+    return axios.delete(`/api/appointments/${id}`, {appointment})
       .then(() => {
         const days = updateSpots(state, false);        
         setState({
